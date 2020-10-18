f6629eb19ac7bfcd6845e39bb441eae64d84f5fb
ASHISH KUMAR <giftofgodashish@gmail.com>
2020-10-18 16:18:26 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
 module.exports = {
   mongoURI:
-    'mongodb://ashish:Ashish9055@mernbasic-shard-00-00-w0ajb.gcp.mongodb.net:27017,mernbasic-shard-00-01-w0ajb.gcp.mongodb.net:27017,mernbasic-shard-00-02-w0ajb.gcp.mongodb.net:27017/test?ssl=true&replicaSet=MERNBasic-shard-0&authSource=admin&retryWrites=true&w=majority',
-};
+    ''
 module.exports = {
   mongoURI: 'mongodb://127.0.0.1:27017',
 };
